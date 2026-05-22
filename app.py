9340c409d4b7e0cab39b81448bc065a0580a5ca0
Abhirag B <bodiabhirag@gmail.com>
2026-05-22 18:31:38 -0500

    added datatype aware analysis for numeric and categorical data and added histogram visualizations

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, render_template
 import os
 import pandas as pd
+import matplotlib.pyplot as plt
 
 app = Flask(__name__)
 
@@ -26,17 +27,38 @@ def upload():
 @app.route("/analyze", methods=["POST"])
 def analyze():
     column = request.form["column"]
-
     col_data = df_global[column]
+    col_data = col_data.dropna()
+    plot_path = None
 
-    stats = {
-        "mean": col_data.mean(),
-        "median": col_data.median(),
-        "std": col_data.std(),
-        "missing": col_data.isnull().sum()
-    }
+    plt.figure()
+    col_data.hist()
 
-    return render_template("analysis.html", column=column, stats=stats)
+    if pd.api.types.is_numeric_dtype(col_data):
+        stats = {
+            "type": "numeric",
+            "mean": col_data.mean(),
+            "median": col_data.median(),
+            "std": col_data.std(),
+            "missing": col_data.isnull().sum()
+        }
+        plot_filename = f"{column}_hist.png"
+        plot_path = os.path.join("static", "plots", plot_filename)
+
+        plt.title(f"{column} Distribtion")
+        plt.xlabel(column)
+        plt.ylabel("Frequency")
+        plt.savefig(plot_path)
+
+        plt.close()
+    else:
+        stats = {
+            "type": "categorial",
+            "unique": col_data.nunique(),
+            "most_common": col_data.mode().iloc[0] if not col_data.mode().empty else None,
+            "missing": df_global[column].isnull().sum()
+        }
+    return render_template("analysis.html", column=column, stats=stats, plot_path=plot_path)
 
 app.run(debug=True)
 
